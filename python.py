2ce6deae280d9e02b48e815511d567a8fbd6e137
Srikanth Pagadala <srikanthpagadala@Srikanths-MacBook-Air.local>
2025-04-29 17:35:40 +0530

    simple_project

---
+++ python.py
@@ -0,0 +1,23 @@
+
+
+# lis = ["srikanth", "nishitha", "srikanth"]
+# remove = "srikanth"
+# n = 2
+# c = 0
+# for i in range(0, len(lis)):
+#     if (lis[i] == remove):
+#         c = c + 1
+#         if (c == n):
+#             del lis[i]
+# print(lis)
+
+
+fac = -1
+num = 5
+
+if fac < 0:
+    print("in valid")
+else:
+    for i in range(1, num + 1):
+        fac = fac * i
+        print(fac)
